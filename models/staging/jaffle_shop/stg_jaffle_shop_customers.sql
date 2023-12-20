f3c4455a1525f2014426df145b243b9a7f309129
Sarah Cree <111320702+SarahCree@users.noreply.github.com>
2023-12-20 09:50:09 -0700

    Update stg_jaffle_shop_customers.sql
    adding configs + tag

--- models/staging/jaffle_shop/stg_jaffle_shop_customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop_customers.sql
@@ -1,3 +1,9 @@
+{{ config(
+    materialized='table',
+    file_format='delta',
+    tags=["test"]
+) }}
+
 with source_customers as (
 
     select * from {{ source('dbt_learning', 'jaffle_shop_customers') }}
